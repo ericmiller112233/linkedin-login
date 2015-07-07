53a142a94022f58b1624954bca8a1e63f56c0fdd
ericmiller112233 <ericmiller112233@gmail.com>
2015-07-06 18:32:31 -0700

    cleaned up code, got rid of global variables

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ import requests
 import Cookie
 import time
 
-linkedin_login(user, pw) # replace with user@email.com and password for account you want to log into.
+linkedin_login(user, pw)
 
 # Login, the user login data will come from a database.
 def linkedin_login(user, pw):
 
-    global url
-    url = "https://www.linkedin.com/uas/login"
+    login_get_url = "https://www.linkedin.com/uas/login"
 
-    get(url)
-    get_login_csrf_param()
-    get_csrf_token()
-    get_source_alias()
+    # Initialize cookies session here.
+    session = requests.Session()
 
-    global payload
+    # Get the html on the login page to grab the tokens needed to log in later.
+    login_get = session.get(login_get_url, verify=False).text
+    soup = BeautifulSoup(login_get)
+    csrf_param = soup.find(id="loginCsrfParam-login")['value']
+    csrf_token = soup.find(id="csrfToken-login")['value']
+    source_alias = soup.find(id="sourceAlias-login")['value']
+
     payload = {
         'isJsEnabled': 'yes',
         'source_app': '',
@@ -35,61 +38,15 @@ def linkedin_login(user, pw):
         'sourceAlias': source_alias
     }
 
-    global url
-    url = "https://www.linkedin.com/uas/login-submit"
-    post(url)
-
-
-def post(url):
-#    http = httplib2.Http()
-
     headers = {'Content-type': 'application/x-www-form-urlencoded'}
     raw = urllib.urlencode(payload)
 
-    # This is the HTML from login post, use to get tokens
-    global POST
-    POST = session.post(url, data=raw, cookies=sessionCookies, headers=headers).text
-    print POST
-    print "^^^that was post"
+    login_post_url = "https://www.linkedin.com/uas/login-submit"
 
-def get(url):
-    # Uses sessions to contain cookies,
-    # see http://docs.python-requests.org/en/latest/user/advanced/#session-objects
-    global session
-    session = requests.Session()
 
-    session.get(url, verify=False)
-    response = session.get(url, verify=False)
-    global sessionCookies
     sessionCookies = session.cookies
 
-    print(response.text)
-    print "^^^that was the response text"
-
-
-    global soup
-    soup = BeautifulSoup(response.text)
-    print (soup.prettify())
-
-
-
-def get_login_csrf_param():
-    global csrf_param
-    csrf_param = soup.find(id="loginCsrfParam-login")['value']
-    print "loginCsrfParam"
-    print csrf_param
-
-def get_csrf_token():
-    global csrf_token
-    csrf_token = soup.find(id="csrfToken-login")['value']
-    print "csrf token:"
-    print csrf_token
-
-    print soup
-
-def get_source_alias():
-    print "^^^that was soup"
-    global source_alias
-    source_alias = soup.find(id="sourceAlias-login")['value']
-    print "sourceAlias:"
-    print source_alias
+    login_post = session.post(login_post_url, data=raw, cookies=sessionCookies, headers=headers).text
+    print login_post
+    print "^^^ that was the post response"
+    soup = BeautifulSoup(login_post)
